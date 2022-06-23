68f0fdb787a78143e5fc47ca9ce189dc0ce85a78
jinach0i <80081194+jinach0i@users.noreply.github.com>
2022-06-23 13:21:56 +0900

    update weather.js

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@ const API_Key="f685ae0410e58f89a90332f327f10bea";
 function onGeoOK(position) {
     const lat=position.coords.latitude,
     lon=position.coords.longitude,
-    url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}&units=metric`;
+    url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}&units=metric&lang=kr`;
     console.log(url);
     fetch(url)
     .then(response=>response.json())
